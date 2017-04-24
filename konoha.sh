838bee97c719b662c565872b63dc88d1571ae39f
Vojta Oupicky <voj.oup@gmail.com>
2017-04-24 16:23:09 +0200

    Zmena vystupu
    Nyni na vystupu pise cislo predchoziho dilu a na dalsi radce cislo
    noveho dilu.

--- konoha.sh
+++ konoha.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-dil=$(cat ~/konoha/cislo_dilu.txt)
+dil=$(cat ~/Scripts/cislo_dilu.txt)
+
+echo "Posledni dil mel cislo : $dil"
 
 vyskyt=$(curl -s "http://147.32.8.168/" | grep -c "Boruto $dil")
 	
 if [ $vyskyt -gt 0 ] 
 then 
-	echo "$(($dil+1))" > ~/konoha/cislo_dilu.txt	
-	echo "Nejspise vysel novy dil Boruta, radsi se podivej..."
+	echo "$(($dil+1))" > ~/Scripts/cislo_dilu.txt	
+	echo "Nejspise vysel novy dil Boruta s cislem $(($dil+1)), radsi se podivej..."
 fi
