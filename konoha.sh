9eb71cc15c43f7fc6bf0a3490ca1f9ce951d1b64
Vojta Oupicky <voj.oup@gmail.com>
2017-04-25 09:00:16 +0200

    Uprava vystupu
    Po spusteni script vypise cislo posledniho dilu a cislo predpokladaneho
    noveho dilu, ktery hleda.
    Pokud uz novy dil vysel, vypise, ze novy dil asi vysel.
    Pokud novy dil jeste nevysel, vypise informaci o tom, ze jeste nevysel a
    pozbudivou vetu abych si pockal :D.

--- konoha.sh
+++ konoha.sh
@@ -2,12 +2,18 @@
 
 dil=$(cat ~/Scripts/cislo_dilu.txt)
 
-echo "Posledni dil mel cislo : $dil"
+echo "Posledni dil mel cislo $(($dil-1))"
+echo "Novy dil by mel mit cislo $dil."
 
 vyskyt=$(curl -s "http://147.32.8.168/" | grep -c "Boruto $dil")
 	
 if [ $vyskyt -gt 0 ] 
 then 
+    echo ""
 	echo "$(($dil+1))" > ~/Scripts/cislo_dilu.txt	
 	echo "Nejspise vysel novy dil Boruta s cislem $(($dil+1)), radsi se podivej..."
+else
+    echo ""
+    echo "Novy dil jeste nejspis nevysel."
+    echo "Jeste chvilku vydrz :)"
 fi
